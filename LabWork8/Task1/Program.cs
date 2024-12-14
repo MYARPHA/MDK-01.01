7c0e5707f4a4d13452c7898a7e3e8db0bd3ce9b8
Alina Myarpha <chtotakoe444@gmail.com>
2024-12-14 15:25:50 +0300

    12/14

---
+++ LabWork8/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using Task1;
+
+Files file = new("hogwarts_sucks.txt", "../asasas/hogwarts_sucks.txt" , 100);
+Console.WriteLine(file);
+Console.WriteLine();
+Console.WriteLine(file.ToString());
+
+Files file1 = new("aboba.png", "../twitchemotes/aboba.png", 123);
+Console.WriteLine(file.Equals(file1));
+
---
+++ LabWork8/Task1/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1
+{
+    class Rectangle : Figure
+    {
+        public int x;
+        public int y;
+
+        public Rectangle(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        
+    }
+}
